f923c3d0fd779379cc4dd3dcee4562f95005eec2
Mark Del Mar <mark@1000martians.com>
2019-10-22 10:23:30 -0400

    finished associations

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,111 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+
+@restaurants = [
+  {
+    name: "Arby's",
+    route: "/arbys",
+    foods: []
+  },
+  {
+    name: "Burger King",
+    route: "/burger-king",
+    foods: []
+  },
+  {
+    name: "Dairy Queen",
+    route: "/dairy-queen",
+    foods: []
+  },
+  {
+    name: "Five Guys",
+    route: "/five-guys",
+    foods: []
+  },
+  {
+    name: "In-N-Out Burger",
+    route: "/in-n-out-burger",
+    foods: []
+  },
+  {
+    name: "Jack in the Box",
+    route: "/jack-in-the-box",
+    foods: []
+  },
+  {
+    name: "KFC",
+    route: "/kfc",
+    foods: []
+  },
+  {
+    name: "McDonald's",
+    route: "/mcdonalds",
+    foods: []
+  },
+  {
+    name: "Panda Express",
+    route: "/panda-express",
+    foods: []
+  },
+  {
+    name: "Popeyes",
+    route: "/popeyes",
+    foods: []
+  },
+  {
+    name: "Smashburger",
+    route: "/smashburger",
+    foods: []
+  },
+  {
+    name: "Sonic",
+    route: "/sonic",
+    foods: []
+  },
+  {
+    name: "Subway",
+    route: "/subway",
+    foods: []
+  },
+  {
+    name: "Taco Bell",
+    route: "/taco-bell",
+    foods: []
+  },
+  {
+    name: "Wendy's",
+    route: "/wendys",
+    foods: []
+  },
+  {
+    name: "Zaxby's",
+    route: "/zaxbys",
+    foods: []
+  },
+]
+
+def scraper
+  url = "https://fastfoodnutrition.org"
+
+  @restaurants.each do |restaurant|
+    unparsed = HTTParty.get(url + restaurant[:route])
+    parsed = Nokogiri::HTML(unparsed)
+
+    unparsed_foods = parsed.css('div.filter_target')
+    unparsed_foods.each do |unparsed_food|
+    parsed_food = unparsed_food.text
+    food = {
+      name: parsed_food.split(' Nutrition Facts').first,
+      image: nil,
+      # TODO: Remove foods with less than 350 calories
+      # TODO: 960-1180 calories (accept average, round to 0)
+      calories: parsed_food.split(' Nutrition Facts').last.split(' calories').last.to_i
+    }
+    restaurant[:foods] << food
+    end
+  end
+
+end
+
+scraper
