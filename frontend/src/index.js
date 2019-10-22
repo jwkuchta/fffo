e38c9f0376800b491f0835b5f01a7d113388b719
Alexandria <alex.pugia816@hotmail.com>
2019-10-22 17:07:48 -0400

    added basic frontend styling

---
+++ frontend/src/index.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navigation = document.querySelector("#navigation-bar")
+    
+    fetchCards()
+})
+
+function fetchCards() {
+    console.log("hi")
+}
+
+
+
+
+
+
+                /* <div class="ui two link cards" style="margin-left: 10%; margin-right: 10%;">
+                    <!-- left/first card -->
+                    <div id="card1" class="ui centered card">
+                            <!-- food photo -->
+                            <div id="card1-photo" class="image"></div>
+                            <div class="content center">   
+                                <!-- food name -->
+                                <div id="card1-food-name" class="header">Item Name</div>
+                                <div class="meta"> --- </div>
+                                <!-- retaurant name -->
+                                <div id="card1-restaurant-name" class="description">Restaurant Name</div>
+                            </div>
+                            <!-- bottom bar -->
+                            <div class="extra content">
+                                <span id="card1-bottom-bar" class="right floated"></span>
+                            </div>
+                        </div>
+
+                    <!-- right/second card -->
+                    <div id="card2" class="ui centered card">
+                        <!-- food photo -->
+                        <div id="card2-photo" class="image"></div>
+                        <div class="content center">   
+                            <!-- food name -->
+                            <div id="card2-food-name" class="header">Item Name</div>
+                            <div class="meta"> --- </div>
+                            <!-- retaurant name -->
+                            <div id="card2-restaurant-name" class="description">Restaurant Name</div>
+                        </div>
+                        <!-- bottom bar -->
+                        <div class="extra content">
+                            <span id="card2-bottom-bar" class="right floated"></span>
+                        </div>
+                    </div>
+                </div> */
